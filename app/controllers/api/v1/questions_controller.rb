468e32b7b8cbc33acd251e329049ecff085319f5
kennankole <kennankole@gmail.com>
2023-11-26 13:59:25 +0300

    Created the questions endpoint to fetch all questions

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+    if @questions.any?
+      question_info = @questions.map do |question|
+        {
+          question: {
+            id: question.id,
+            body: question.body,
+            date_time: question.created_at,
+            user_identity: question.user.present? ? question.user.user_identity : nil,
+            # answer: question.user.answer.present? ? question.user.answer : nil
+          }
+        }
+      end
+      render json: question_info
+    else
+      render json: {
+        message: 'No questions found'
+      }, status: :not_found
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    registration: 'signup'
+    registration: 'signup',
+    
   },
   controllers: {
     sessions: 'api/v1/users/sessions',
     registrations: 'api/v1/users/registrations'
   }
   get 'current_user/index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :questions, only: [:index, :show]
+    end
+  end
 end
